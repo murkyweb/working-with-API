2705bb01e7f3ec0e4231b3f476387eef03268eb4
Alexandru <murkyweb@protonmail.com>
2022-06-20 16:20:37 +0300

    Added button to fetch new image without refreshing the page

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const img = document.querySelector("img");
+const button = document.querySelector("button");
 
-fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=LeFroF97wlzZOFiy7Kq0XAF65JEWNXU2&s=cats",
-  {
-    mode: "cors",
-  }
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    img.src = response.data.images.original.url;
-  });
+const getImage = () => {
+  fetch(
+    "https://api.giphy.com/v1/gifs/translate?api_key=LeFroF97wlzZOFiy7Kq0XAF65JEWNXU2&s=cats",
+    {
+      mode: "cors",
+    }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      img.src = response.data.images.original.url;
+    });
+};
+
+getImage();
+button.addEventListener("click", getImage);
